bb70355b490c85f65b2de31426cdccd70e3a1752
21wh1a6618 <96616374+21wh1a6618@users.noreply.github.com>
2022-04-07 20:40:56 +0530

    Add files via upload

---
+++ my captain area of circle new.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+radius=float(input("enter the radius of circle:"))
+enter the radius of circle:1.1
+area=3.14*radius*radius
+print("area of circle is:",area)
+area of circle is: 3.7994000000000008
+#second code
+filename=input("enter the file name:")
+enter the file name:abc.py
+file_extension="python"
+print("the extension of the file is :",file extension)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print("the extension of the file is :",file_extension)
+the extension of the file is : python
